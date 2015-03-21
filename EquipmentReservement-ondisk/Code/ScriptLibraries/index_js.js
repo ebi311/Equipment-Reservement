d69ec445ed8eaf16e5fb079dc909c3c5c89a31f4
EBIHARA Kenji <ebi311@gmail.com>
2015-03-22 00:00:19 +0900

    登録ダイアログまで

--- EquipmentReservement-ondisk/Code/ScriptLibraries/index_js.js
+++ EquipmentReservement-ondisk/Code/ScriptLibraries/index_js.js
@@ -9,10 +9,11 @@ $(document).ready(function() {
     	    right:  'today prev,next, month, agendaWeek, agendaDay'
     	},
     	defaultView: "agendaDay",
+    	allDaySlot : false,
     	firstDay : 1,
     	timeFormat: {
-    	    agenda: 'HH:mm{ - HH:mm}', // 5:00 - 6:30
-    	    '': 'HH(:mm)'            // 7p
+    	    agenda: 'HH:mm{ - HH:mm}',
+    	    '': 'HH(:mm)'
     	},
     	dayNames :["日曜日","月曜日","火曜日","水曜日","木曜日","金曜日","土曜日"],
     	dayNamesShort :["日","月","火","水","木","金","土"],
@@ -20,7 +21,7 @@ $(document).ready(function() {
     	monthNamesShort  : ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],
     	titleFormat :{
     	    month: 'yyyy / M',
-    	    week: "yyyy/M/d { '&#8212;'yyyy/M/d}",
+    	    week: "yyyy/M/d { - yyyy/M/d}",
     	    day: 'yyyy/M/d dddd'
     	},
     	columnFormat :{
@@ -35,7 +36,50 @@ $(document).ready(function() {
     	axisFormat : "HH:mm",
     	slotEventOverlap : false,
     	selectable : true,
-    	selectHelper : true
-    })
+    	unselectAuto : false,
+    	selectHelper : true,
+    	hiddenDays : [0,6],
+    	select : calendar.select
+    });
 
+    $('#reservatiomModal_date').datetimepicker(calendar.datePickerOption);
+    $('#reservatiomModal_startTime').datetimepicker(calendar.timePickerOption);
+    $('#reservatiomModal_endTime').datetimepicker(calendar.timePickerOption);
+    
+    $('#reservatiomModal').on("show.bs.modal", function(e){
+    });
+    $('#reservatiomModal').on("hide.bs.modal", function(e){
+    	$('#calendar').fullCalendar( 'unselect' );
+    });
 });
+var calendar ={};
+calendar.datetimeFormatString = "yyyy/MM/dd HH:mm";
+calendar.dateFormatString = "yyyy/MM/dd";
+calendar.timeFormatString = "HH:mm";
+calendar.datePickerOption = {
+	timepicker: false,
+	lang: "ja"
+};
+calendar.timePickerOption = {
+	datepicker:false,
+	format:'H:i',
+	lang: "ja",
+	step: 30,
+	minTime: "08:00",
+	maxTime: "20:00"
+};
+calendar.select = function select(startDate, endDate, allDay, jsEvent, view){
+	$('#reservatiomModal').modal();
+	$("#reservatiomModal_equipment").val("-1");
+	var dateStr = 
+		$.fullCalendar.formatDate( startDate, calendar.dateFormatString);
+	$("#reservatiomModal_date").val(dateStr);
+	var startTimeStr = 
+		$.fullCalendar.formatDate( startDate, calendar.timeFormatString);
+	$("#reservatiomModal_startTime").val(startTimeStr);
+	var endTimeStr = 
+		$.fullCalendar.formatDate( endDate, calendar.timeFormatString);
+	$("#reservatiomModal_endTime").val(endTimeStr);
+	$("#reservatiomModal_subscriber").val("");
+	$("#reservatiomModal_note").val("");
+}
